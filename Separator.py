cd8984d1a055d1de255edabf5457c0f7052d0f7a
hunganh <scramblegf@gmail.com>
2019-12-15 21:44:41 +0200

    Fix & comment separator file, add testPerformance file to test the separation quality

--- Separator.py
+++ Separator.py
@@ -1,59 +1,75 @@
 import numpy as np
 import librosa as lb
-from scipy.signal import stft
-from scipy.signal import istft
+#from scipy.signal import stft, istft
 import math
 
-
-def auxiliary(a, H, P):
+def find_delta(a, H, P):
+    """
+    Find the delta value to update harmonic and percussive power spectrograms
+    @ params: a: float - controls the weight of harmonic and percussive components
+              H: 2D numpy array - power spectrum of harmonic component
+              P: 2D numpy array - power spectrum of percussive component
+    """
     H = np.c_[np.zeros((np.size(H, 0),), dtype=float), H, np.zeros((np.size(H, 0),), dtype=float)]
     P = np.r_[[np.zeros((np.size(P, 1),), dtype=float)], P, [np.zeros((np.size(P, 1),), dtype=float)]]
 
-    delta = a*(H[:, :-2] - 2*(H[:, 1:-1]) + H[:, 2:]) - (1 - a)*(P[:-2, :] - 2*(P[1:-1, :]) + P[2:, :])
+    delta = a*(H[:, :-2] - 2*(H[:, 1:-1]) + H[:, 2:])/4 - (1 - a)*(P[:-2, :] - 2*(P[1:-1, :]) + P[2:, :])/4
 
     return delta
 
-# load the signal
-audioIn, sr = lb.load("project_test1.wav")
+def separate(filename, y=1, a_h=1, a_p=1, k_max=20):
+    """
+    Separate an audio file into 2 audio files, one for percussive components (drums)
+    and one for harmonic components (singing & others)
+    @params: filename: string - input audio file
+             y: float (0 < y <= 1) - range compression coefficient, facilitates the separation
+             a_h and a_p: float - control the weights of the horizontal and vertical smoothness
+             k_max: number of iterations
 
-# perform short-time Fourier transform
-F = stft(audioIn, sr)[2]
+    @return: 2 audio files "H.wav" and "P.wav" are created, which contain harmonic and
+             percussive components, respoectively, and saved into the current directory.
+    """
+    # load the signal
+    audioIn, sr = lb.load(filename, sr=None)
 
-y = 1
+    # perform short-time Fourier transform
+    n_fft = 2048
+    n_audio = len(audioIn)
+    # make sure that the signal length will not be trimmed after stft and istft
+    audioIn_pad = lb.util.fix_length(audioIn, n_audio + n_fft // 2)
+    F = lb.stft(audioIn_pad, n_fft=n_fft)
 
-# Calculate power spectrogram
-W = np.abs(F)**(2*y)
+    # Calculate a range-compressed version of the power spectrogram
+    W = np.abs(F)**(2*y)
 
-#set initial values
-H = W/2
-P = W/2
-x = np.zeros((np.size(P, 1),), dtype=float)
-k = 0
-k_max = 5
-a_h = 1
-a_p = 1
-a = (a_p**2)/(a_h**2 + a_p**2)
+    # Set initial values for harmonic and percussive power spectrogram
+    H = W/2
+    P = W/2
+    k = 0
+    a = (a_p**2)/(a_h**2 + a_p**2)
 
-zeros_H = np.zeros_like(H)
-while k < k_max - 1:
-    delta = auxiliary(a, H, P)
-    
-    #H = min(max(H + delta, zeros_H), W)
-    H = H + delta
-    H [ H < 0 ] = 0
-    np.copyto(H, W, where = H > W)
-    
-    P = W - H
-    k += 1
-    
+    while k < k_max - 1:
+        delta = find_delta(a, H, P)
+        
+        #H = min(max(H + delta, 0), W)
+        H = H + delta
+        H [ H < 0 ] = 0
+        np.copyto(H, W, where = H > W)
+        
+        P = W - H
+        k += 1
+        
+    # binarize separation output
+    H = np.multiply((H >= P).astype('int'), W)
+    P = np.multiply((P > H).astype('int'), W)
+    # convert separated power spectrums back to waveform signals
+    # by inverse short time Fourier transform
+    x_h = lb.istft(H**(1/2*y) * math.e**(1j*np.angle(F)), length=n_audio)
+    x_p = lb.istft(P**(1/2*y) * math.e**(1j*np.angle(F)), length=n_audio)
 
+    lb.output.write_wav("H.wav", x_h, sr, norm=False)
+    lb.output.write_wav("P.wav", x_p, sr, norm=False)
 
-H = np.multiply((H >= P).astype('int'), W)
-P = np.multiply((P > H).astype('int'), W)
-
-t_h, x_h = istft(H**(1/2*y) * math.e**(1j*np.angle(F)))
-t_p, x_p = istft(P**(1/2*y) * math.e**(1j*np.angle(F)))
-
-lb.output.write_wav("H.wav", x_h, sr, norm=False)
-lb.output.write_wav("P.wav", x_p, sr, norm=False)
-
+police = 'police03short.wav' # k = 20, y = 1
+project = 'project_test1.wav'
+separate(project)
